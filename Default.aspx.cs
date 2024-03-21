e5764a3d681ac6b9f2ff9b3645aaf8b660b04aa6
ryan2625 <ryancfreas@gmail.com>
2024-03-21 08:21:05 -0500

    Update storing ArrayList in session to persist state

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -11,29 +11,29 @@ namespace asp_app
     public partial class _Default : Page
     {
 
-        public ArrayList animals = new ArrayList();
+        private ArrayList animals;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            lbl.Text = "TESTINGG 2";
-            ArrayList values = new ArrayList();
 
-            values.Add("Tree");
-            values.Add("Bark");
-            values.Add("Wood");
-
-            Repeater1.DataSource = values;
-            Repeater1.DataBind();
-
-            string[] items = { "Ant", "Dog", "Cat" };
-
-            foreach (string item in items)
+            if (!IsPostBack)
             {
-                animals.Add(item);
-            }
-            Repeater_Binder(animals, AnimalRepeater);
-        }
+                animals = new ArrayList();
+                Session["Animals"] = animals;
+                string[] items = { "Ant", "Dog", "Cat" };
 
+                foreach (string item in items)
+                {
+                    animals.Add(item);
+                }
+                Repeater_Binder(animals, AnimalRepeater);
+
+            } else
+            {
+                animals = (ArrayList)Session["Animals"];
+                Repeater_Binder(animals, AnimalRepeater);
+            }
+        }
 
         public void Repeater_Binder(ArrayList param1, Repeater repeater1)
         {
